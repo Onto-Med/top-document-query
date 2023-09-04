edcfb520d11a345fbdd8098112e03fed127bc73b
Franz Matthies <fmatthies@users.noreply.github.com>
2023-09-04 11:29:03 +0200

    added basic cvs storage functionality

---
+++ src/main/java/care/smith/top/top_document_query/converter/csv/DocumentCSV.java
@@ -0,0 +1,52 @@
+package care.smith.top.top_document_query.converter.csv;
+
+
+import care.smith.top.model.Concept;
+import care.smith.top.model.Entity;
+import care.smith.top.top_document_query.util.Entities;
+
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class DocumentCSV {
+
+  private Charset charset = StandardCharsets.UTF_8;
+  private String entriesDelimiter = ";";
+  private String entryPartsDelimiter = ",";
+  private String language = null;
+
+  public DocumentCSV() {}
+
+  public DocumentCSV language(String language) {
+    this.language = language;
+    return this;
+  }
+
+  public DocumentCSV charset(Charset charset) {
+    this.charset = charset;
+    return this;
+  }
+
+  public DocumentCSV charset(String charset) {
+    return charset(Charset.forName(charset));
+  }
+
+  public DocumentCSV entriesDelimiter(String entriesDelimiter) {
+    this.entriesDelimiter = entriesDelimiter;
+    return this;
+  }
+
+  public DocumentCSV entryPartsDelimiter(String entryPartsDelimiter) {
+    this.entryPartsDelimiter = entryPartsDelimiter;
+    return this;
+  }
+
+  public void write(Entity[] concepts, OutputStream outputStream){
+    CSVWriter writer = new CSVWriter(outputStream, entriesDelimiter, charset);
+    writer.write(CSVMetadataRecord.FIELDS);
+    for (Concept con : Entities.of(concepts).getConcepts())
+      writer.write(new CSVMetadataRecord(con, concepts, entryPartsDelimiter, language));
+    writer.flush();
+  }
+}
