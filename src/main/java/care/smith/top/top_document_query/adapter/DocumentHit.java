483b658fa02d004da9ca55d448bdb3eb68cd5d82
Franz Matthies <fmatthies@users.noreply.github.com>
2024-04-20 17:11:44 +0200

    added ES-highlighting to SO queries -> highlights are now used as extract value when saving results to csv

--- src/main/java/care/smith/top/top_document_query/adapter/DocumentHit.java
+++ src/main/java/care/smith/top/top_document_query/adapter/DocumentHit.java
@@ -2,21 +2,29 @@ package care.smith.top.top_document_query.adapter;
 
 import care.smith.top.top_document_query.elasticsearch.DocumentEntity;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
 public class DocumentHit {
   private DocumentEntity document;
   private String documentId;
+  private Map<String, List<String>> highlights;
   private double score;
 
   public DocumentHit(String documentId, DocumentEntity document) {
     this.setDocumentId(documentId);
     this.setDocument(document);
     this.setScore(0.0);
+    this.setHighlights(null);
   }
 
-  public DocumentHit(String documentId, DocumentEntity document, Double score) {
+  public DocumentHit(String documentId, DocumentEntity document, Map<String, List<String>> highlights, Double score) {
     this.setDocumentId(documentId);
     this.setDocument(document);
     this.setScore(score);
+    this.setHighlights(highlights);
   }
 
   public DocumentEntity getDocument() {
@@ -43,6 +51,15 @@ public class DocumentHit {
 
   public DocumentHit setScore(Double score) {
     this.score = score != null ? score : 0.0;
+    return this;
+  }
+
+  public Map<String, List<String>> getHighlights() {
+    return highlights;
+  }
+
+  public DocumentHit setHighlights(Map<String, List<String>> highlights) {
+    this.highlights = (highlights != null && !highlights.isEmpty()) ? highlights : null;
     return this;
   }
 }
